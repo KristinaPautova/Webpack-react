9e53933118705df0c25c3136c44dec02bf37aa66
KristinaPautova <hristina1991@gmail.com>
2021-03-05 11:29:28 +0600

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import "../styles/Main.css"
+
+class Main extends Component {
+    render() {
+        return(
+            <main>
+                <div>
+                    <h1>Hello World</h1>
+                </div>
+            </main>
+        )
+    }
+
+}
+
+export default Main;
